051b189e2df539545c092da96cc15e95a40bc7f1
Gavin Rosenthal <38087728+Mullets-Gavin@users.noreply.github.com>
2020-08-22 23:39:04 -0400

    first commit

---
+++ src/ReplicatedStorage/DiceViewport/init.lua
@@ -0,0 +1,141 @@
+--[[
+	@Author: Gavin "Mullets" Rosenthal
+	@Desc: Viewport module to create proper viewports with sizing
+--]]
+
+--[[
+	Variations of call:
+	
+	:Create(element,obj)
+	:Create(element,obj,props)
+	
+	Returns:
+	false or viewport
+	
+	props = {
+		['Distance] = 1; -- distance of the camera to the port
+		['FoV'] = 50; -- field of view
+		['Angles] = CFrame.Angles(0,0,0)
+		['Offset] = CFrame.new(0,0,0)
+	}
+--]]
+
+--// logic
+local Viewport = {}
+Viewport.Cache = {}
+Viewport.Types = {'Frame','ImageLabel','ImageButton','TextButton','TextLabel','TextBox'}
+
+--// services
+local Services = setmetatable({}, {__index = function(cache, serviceName)
+    cache[serviceName] = game:GetService(serviceName)
+    return cache[serviceName]
+end})
+
+--// functions
+local function CreateFolder()
+	local new = Instance.new('Folder')
+	new.Name = 'DiceViewport'
+	new.Parent = Services['ReplicatedStorage']
+	return new
+end
+
+local function CreateViewport(port)
+	local flag = false
+	for index,type in pairs(Viewport.Types) do
+		if port:IsA(type) then
+			flag = true
+			break
+		end
+	end
+	if not flag then
+		warn('[VIEWPORT]: Viewport element only takes frames, imageLabels, imageButton, textbuttons, textlabels or textboxs. You used:',port.ClassName)
+		return false
+	end
+	local new = Instance.new('ViewportFrame')
+	new.Size = UDim2.new(1, 0, 1, 0)
+	new.Position = UDim2.new(0.5, 0, 0.5, 0)
+	new.AnchorPoint = Vector2.new(0.5, 0.5)
+	new.BackgroundTransparency = 1
+	new.ImageColor3 = port.BackgroundColor3
+	if port:IsA('ImageButton') or port:IsA('ImageLabel') then
+		port.Image = ''
+	end
+	new.Parent = port
+	port = new
+	return port
+end
+
+local function CreateObject(clone)
+	if not clone:IsA('Model') and not clone:IsA('BasePart') then
+		warn('[VIEWPORT]: Viewport only takes models or baseparts. You used:',clone.ClassName)
+		return false
+	end
+	local item = clone:Clone()
+	if not item:IsA('Model') then
+		local folder = CreateFolder()
+		local new = Instance.new('Model')
+		item.Parent = folder
+		for index,part in pairs(folder:GetChildren()) do
+			if part:IsA('BasePart') then
+				part.Parent = new
+				new.PrimaryPart = part
+			end
+		end
+		item = new
+		folder:Destroy()
+	end
+	return item
+end
+
+function Viewport:Spin(element)
+	
+end
+
+function Viewport:Destroy(element)
+	if Viewport.Cache[element] then
+		Viewport.Cache[element]['Element']:Destroy()
+		for index,event in pairs(Viewport.Cache[element]['Events']) do
+			event:Disconnect()
+		end
+		Viewport.Cache[element] = nil
+	end
+end
+
+function Viewport:Create(element,obj,props)
+	if not element then warn() return false end
+	if not props then props = {} end
+	if Viewport.Cache[element] then
+		Viewport.Cache[element]['Element']:Destroy()
+		for index,event in pairs(Viewport.Cache[element]['Events']) do
+			event:Disconnect()
+		end
+		Viewport.Cache[element] = nil
+	end
+	local item = CreateObject(obj)
+	local port = CreateViewport(element)
+	if item and port then
+		item.Parent = port
+		local size = item:GetExtentsSize()
+		local dist = props['Distance'] or 1
+		local fov = props['FoV'] or 50
+		local angles = props['Angles'] or CFrame.Angles(0,0,0)
+		local offset = props['Offset'] or CFrame.new(0,0,0)
+		local camera = Instance.new('Camera') do
+			local cframe = item.PrimaryPart.CFrame * angles * CFrame.new(0,0,size.Y * dist) * offset
+			camera.FieldOfView = fov
+			camera.CameraType = Enum.CameraType.Scriptable
+			camera.CFrame = cframe
+			camera.Parent = port
+		end
+		port.CurrentCamera = camera
+		Viewport.Cache[element] = {
+			['Element'] = port;
+			['Events'] = {};
+		}
+		return port
+	end
+	warn('[VIEWPORT]: Failed to create viewport, missing item and/or port')
+	return false
+end
+
+return Viewport
